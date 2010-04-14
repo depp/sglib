be01d1bd3dfcadd9e133ca309ecf2972c372971a
Dietrich Epp <depp@zdome.net>
2010-04-13 19:08:51 -0700

    Simple movement now works in response to input.

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,26 @@
 #include "SDL_opengl.h"
 #include <cmath>
 
+const float kPi = 4.0f * std::atan(1.0f);
+const float kPlayerForwardSpeed = 0.4f;
+const float kPlayerTurnSpeed = 10.0f;
+float playerX = 0.0f, playerY = 0.0f, playerFace = 0.0f;
+
+float playerMove(float d)
+{
+    playerX += d * cos(playerFace * (kPi / 180.0f));
+    playerY += d * sin(playerFace * (kPi / 180.0f));
+}
+
+float playerTurn(float a)
+{
+    playerFace += a;
+    if (playerFace >= 360.0f)
+        playerFace -= 360.0f;
+    else if (playerFace < 0.0f)
+        playerFace += 360.0f;
+}
+
 void drawScene(void)
 {
     int x, y;
@@ -10,14 +30,18 @@ void drawScene(void)
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glFrustum(-0.1f, 0.1f, -0.075f, 0.075f, 0.1f, 100.0f);
-    glRotatef(90.0f, 1.0f, 0.0f, 0.0f);
-    glTranslatef(0.0f, 0.0f, 1.0f);
+    glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
+    glRotatef(90.0f - playerFace, 0.0f, 0.0f, 1.0f);
+    glTranslatef(-playerX, -playerY, -1.0f);
     glMatrixMode(GL_MODELVIEW);
 
-    glColor3f(1.0f, 0.0f, 0.0f);
     glBegin(GL_TRIANGLES);
     for (x = -5; x <= 5; ++x) {
         for (y = -5; y <= 5; ++y) {
+            if (x == 1 && y == 0)
+                glColor3f(0.0f, 1.0f, 0.0f);
+            else
+                glColor3f(1.0f, 0.0f, 0.0f);
             glVertex3f(2*x - 0.5f, 2*y - 0.7f, 0.0f);
             glVertex3f(2*x + 0.5f, 2*y - 0.7f, 0.0f);
             glVertex3f(2*x + 0.0f, 2*y + 0.7f, 0.0f);
@@ -34,6 +58,22 @@ void handleKey(SDL_keysym *key)
     case SDLK_ESCAPE:
         SDL_Quit();
         exit(0);
+        break;
+    case SDLK_UP:
+    case SDLK_w:
+        playerMove(kPlayerForwardSpeed);
+        break;
+    case SDLK_DOWN:
+    case SDLK_s:
+        playerMove(-kPlayerForwardSpeed);
+        break;
+    case SDLK_LEFT:
+    case SDLK_a:
+        playerTurn(kPlayerTurnSpeed);
+        break;
+    case SDLK_RIGHT:
+    case SDLK_d:
+        playerTurn(-kPlayerTurnSpeed);
         break;
     default:
         break;
