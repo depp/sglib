d59d403154c55a9c3bdb4c6a5df965eb8296d17d
Dietrich Epp <depp@zdome.net>
2010-10-07 01:30:14 -0700

    Buttons now draw a box around themselves.

--- src/ui/button.cpp
+++ src/ui/button.cpp
@@ -20,13 +20,24 @@ void UI::Button::setText(std::string const &text)
 
 void UI::Button::draw()
 {
+    glPushAttrib(GL_COLOR_BUFFER_BIT | GL_CURRENT_BIT);
     glPushMatrix();
     glTranslatef(x_, y_, 0.0f);
-    glPushAttrib(GL_COLOR_BUFFER_BIT | GL_CURRENT_BIT);
+
+    glColor3ub(255, 0, 0);
+
+    float x1 = -5.0f, x2 = 150.0f, y1 = -10.0f, y2 = 20.0f;
+    glBegin(GL_LINE_LOOP);
+    glVertex2f(x1, y1);
+    glVertex2f(x1, y2);
+    glVertex2f(x2, y2);
+    glVertex2f(x2, y1);
+    glEnd();
+
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-    glColor3ub(255, 0, 0);
     title_.draw();
+
     glPopAttrib();
     glPopMatrix();
 }
