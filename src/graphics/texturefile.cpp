79acb65a4069dc28b47e338e9688483fc4e65343
Dietrich Epp <depp@zdome.net>
2011-03-11 01:04:25 -0800

    TextureFile now loads using IFile, improved C++ error robustness.

--- src/graphics/texturefile.cpp
+++ src/graphics/texturefile.cpp
@@ -1,4 +1,6 @@
 #include "texturefile.hpp"
+#include "sys/ifile.hpp"
+#include "sys/path.hpp"
 #include <stdlib.h>
 #include <stdio.h>
 #include <err.h>
@@ -6,6 +8,7 @@
 #include <png.h>
 #include <set>
 #include <memory>
+#include <vector>
 #include "SDL_opengl.h"
 
 static const std::string filePrefix("file:");
@@ -65,43 +68,78 @@ bool TextureFile::load()
     return false;
 }
 
+struct PNGStruct {
+    PNGStruct() : png(0), info(0) { }
+    ~PNGStruct() { png_destroy_read_struct(&png, &info, 0); }
+    png_structp png;
+    png_infop info;
+};
+
+static void textureFileRead(png_structp pngp,
+                            png_bytep data, png_size_t length) throw ()
+{
+    try {
+        IFile *f = reinterpret_cast<IFile *>(png_get_io_ptr(pngp));
+        size_t pos = 0;
+        while (pos < length) {
+            size_t amt = f->read(data + pos, length - pos);
+            if (amt == 0) {
+                fputs("Unexpected end of file\n", stderr);
+                longjmp(png_jmpbuf(pngp), 1);
+            }
+            pos += amt;
+        }
+    } catch (std::exception const &e) {
+        fprintf(stderr, "Error: %s\n", e.what());
+        longjmp(png_jmpbuf(pngp), 1);
+    } catch (...) {
+        fputs("Unknown error\n", stderr);
+        longjmp(png_jmpbuf(pngp), 1);
+    }
+}
+
 // FIXME error handling
 bool TextureFile::loadPNG()
 {
-    png_structp pngp = NULL;
-    png_infop infop = NULL;
+    std::auto_ptr<IFile> f;
+    PNGStruct ps;
     int depth, ctype;
-    char const *path = path_.c_str();
-    FILE *f = NULL;
     png_uint_32 width, height;
     uint32_t twidth, theight, i, rowbytes, chan;
     bool color = false, alpha = false;
     unsigned char *data = NULL;
-    png_bytepp rows = NULL;
+    std::vector<png_bytep> rows;
 
-    f = fopen(path, "rb");
-    if (!f) goto syserr;
+    f.reset(Path::openIFile(path_));
 
-    pngp = png_create_read_struct_2(
+    ps.png = png_create_read_struct_2(
         PNG_LIBPNG_VER_STRING,
         NULL, NULL, NULL, NULL, NULL, NULL);
-    if (!pngp) goto pngerr;
-    infop = png_create_info_struct(pngp);
-    if (!infop) goto pngerr;
-    png_init_io(pngp, f);
+    if (!ps.png) goto pngerr;
+    ps.info = png_create_info_struct(ps.png);
+    if (!ps.info) {
+    pngerr:
+        fputs("Failed to initialize LibPNG\n", stderr);
+        return false;
+    }
 
-    png_read_info(pngp, infop);
-    png_get_IHDR(pngp, infop, &width, &height,
+    if (setjmp(png_jmpbuf(ps.png)))
+        return false;
+
+    png_set_read_fn(ps.png, f.get(), textureFileRead);
+
+    png_read_info(ps.png, ps.info);
+    png_get_IHDR(ps.png, ps.info, &width, &height,
                  &depth, &ctype, NULL, NULL, NULL);
 
     switch (ctype) {
     case PNG_COLOR_TYPE_PALETTE:
         color = true;
-        png_set_palette_to_rgb(pngp);
+        png_set_palette_to_rgb(ps.png);
         break;
     case PNG_COLOR_TYPE_GRAY:
         if (depth < 8)
-            png_set_expand_gray_1_2_4_to_8(pngp);
+            png_set_expand_gray_1_2_4_to_8(ps.png);
         break;
     case PNG_COLOR_TYPE_GRAY_ALPHA:
         alpha = true;
@@ -115,7 +153,7 @@ bool TextureFile::loadPNG()
         break;
     }
     if (depth > 8)
-        png_set_strip_16(pngp);
+        png_set_strip_16(ps.png);
 
     alloc(width, height, color, alpha);
     twidth = this->twidth();
@@ -131,26 +169,10 @@ bool TextureFile::loadPNG()
         i = height;
     for (; i < theight; ++i)
         memset(data + rowbytes * i, 0, rowbytes);
-    rows = reinterpret_cast<png_bytepp>(malloc(sizeof(*rows) * height));
-    if (!rows) goto syserr;
+    rows.resize(height);
     for (i = 0; i < height; ++i)
         rows[i] = data + rowbytes * i;
-    png_read_image(pngp, rows);
-    free(rows);
-    rows = NULL;
-    png_read_end(pngp, NULL);
-    png_destroy_read_struct(&pngp, &infop, NULL);
-
+    png_read_image(ps.png, &rows.front());
+    png_read_end(ps.png, NULL);
     return true;
-pngerr:
-    fprintf(stderr, "Could not load texture '%s'", path);
-    goto fail;
-syserr:
-    warn("Could not load texture '%s'", path);
-    goto fail;
-fail:
-    if (f) fclose(f);
-    png_destroy_read_struct(&pngp, &infop, NULL);
-    free(rows);
-    return false;
 }
