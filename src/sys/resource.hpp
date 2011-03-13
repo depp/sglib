b687486a6e4df1c1c347af74286d07e5f3d27c51
Dietrich Epp <depp@zdome.net>
2011-03-13 16:03:10 -0700

    Added generic "Resource" class.

---
+++ src/sys/resource.hpp
@@ -0,0 +1,35 @@
+#ifndef SYS_RESOURCE_HPP
+#define SYS_RESOURCE_HPP
+#include <string>
+
+class Resource {
+public:
+    static void loadAll();
+
+    virtual std::string name() const = 0;
+
+    unsigned int refcount() const { return refcount_; }
+    bool loaded() const { return loaded_; }
+    void markDirty() { loaded_ = false; }
+    void incref() { refcount_++; }
+    void decref() { refcount_--; }
+
+protected:
+    Resource()
+        : loaded_(false), registered_(false), refcount_(0)
+    { }
+    virtual ~Resource();
+
+    void registerResource();
+    virtual void loadResource() = 0;
+    virtual void unloadResource() = 0;
+
+private:
+    Resource(Resource const &);
+    Resource &operator=(Resource const &);
+
+    bool loaded_, registered_;
+    unsigned int refcount_;
+};
+
+#endif
