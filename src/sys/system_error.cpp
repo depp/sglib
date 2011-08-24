6717592cf6c14b1c58dccd402c6d0622d8bf6fb8
Dietrich Epp <depp@zdome.net>
2011-08-23 18:39:09 -0700

    Fix system_error on Debian

--- src/sys/system_error.cpp
+++ src/sys/system_error.cpp
@@ -1,6 +1,3 @@
-// SDL contaminates the build system with a _GNU_SOURCE def,
-// which causes a non-portable strerror_r to be declared.
-#undef _GNU_SOURCE
 #include "system_error.hpp"
 #include <string.h>
 
@@ -11,10 +8,16 @@ char const *system_error::what() const throw()
     try {
         if (what_.empty()) {
             char buf[256];
+#if defined(__GLIBC__)
+            char *r = strerror_r(errno_, buf, sizeof(buf));
+            if (r)
+                what_ = r;
+#else
             int r = strerror_r(errno_, buf, sizeof(buf));
             if (!r)
                 what_ = buf;
+#endif
         }
-    } catch (std::exception &e) { }
+    } catch (std::exception &) { }
     return what_.c_str();
 }
