faed042e0ba04164c78882dd81613a9decf5118b
Dietrich Epp <depp@zdome.net>
2011-12-14 22:56:07 -0800

    Coalesce clock implementations into one file

--- src/sys/clock.cpp
+++ src/sys/clock.cpp
@@ -1,3 +1,25 @@
+#if defined(__APPLE__)
+#include <mach/mach_time.h>
+#include <time.h>
+
+static uint64_t ClockZero;
+static struct mach_timebase_info ClockInfo;
+
+void initTime(void)
+{
+    mach_timebase_info(&ClockInfo);
+    ClockZero = mach_absolute_time();
+}
+
+unsigned getTime(void)
+{
+    uint64_t tv = mach_absolute_time();
+    return (tv - ClockZero) * ClockInfo.numer
+        / ((uint64_t) ClockInfo.denom * 1000000);
+}
+
+#else
+
 #include <time.h>
 #include <unistd.h>
 #if _POSIX_TIMERS && defined(_POSIX_MONOTONIC_CLOCK)
@@ -20,7 +42,6 @@ unsigned getTime()
 #else
 #include <sys/time.h>
 
-
 static struct timeval ClockZero;
 
 void initTime()
@@ -35,5 +56,7 @@ unsigned getTime()
     return (tv.tv_sec - ClockZero.tv_sec) * 1000
         + (tv.tv_usec - ClockZero.tv_usec) / 1000;
 }
+
+#endif
 
 #endif
