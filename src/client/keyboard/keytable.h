5637f793fd387bb83066c5a2cf29efc9341fa520
Dietrich Epp <depp@zdome.net>
2011-12-16 03:16:41 -0800

    Add prototype for Windows keycode table

--- src/client/keyboard/keytable.h
+++ src/client/keyboard/keytable.h
@@ -10,6 +10,9 @@ extern const unsigned char MAC_NATIVE_TO_HID[128];
 extern const unsigned char EVDEV_HID_TO_NATIVE[256];
 extern const unsigned char EVDEV_NATIVE_TO_HID[256];
 
+extern const unsigned char WIN_HID_TO_NATIVE[256];
+extern const unsigned char WIN_NATIVE_TO_HID[256];
+
 #ifdef __cplusplus
 }
 #endif
