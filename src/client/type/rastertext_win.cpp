fd36429f1ecaee89c5bb84b5cc3b1cefe4cb4f82
Dietrich Epp <depp@zdome.net>
2011-12-16 00:52:23 -0800

    Add placeholder font code for Windows

--- src/client/type/rastertext_win.cpp
+++ src/client/type/rastertext_win.cpp
@@ -1,5 +1,17 @@
 #include "rastertext.hpp"
 
+// FIXME: unimplemented
+Font::Font(Font const &f)
+{ }
+
+Font::~Font()
+{ }
+
+Font &Font::operator=(Font const &f)
+{
+    return *this;
+}
+
 bool RasterText::loadTexture()
 {
     return true;
