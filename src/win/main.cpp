446f79638c34f509d4308c1d95b509ac9cf12f10
Dietrich Epp <depp@zdome.net>
2011-12-15 22:07:48 -0800

    Begin Windows port

---
+++ src/win/main.cpp
@@ -0,0 +1,231 @@
+#include <windows.h>
+#include <gl\gl.h>
+#include <gl\glu.h>
+
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+
+static HDC hDC;
+static HGLRC hRC;
+static HWND hWnd;
+static HINSTANCE hInstance;
+
+static bool inactive;
+
+static LRESULT CALLBACK wndProc(HWND, UINT, WPARAM, LPARAM);
+
+static void errorBox(const char *str)
+{
+    MessageBox(NULL, str, "ERROR", MB_OK | MB_ICONEXCLAMATION);
+}
+
+static void serrorBox(const char *str)
+{
+    MessageBox(NULL, str, "SHUTDOWN ERROR", MB_OK | MB_ICONINFORMATION);
+}
+
+static void handleResize(int width, int height)
+{
+    if (!height)
+        height = 1;
+    if (!width)
+        width = 1;
+    glViewport(0, 0, width, height);
+}
+
+static void handleRedraw()
+{
+    glClearColor(0.5, 0.0, 0.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+static void killGLWindow()
+{
+    if (hRC) {
+        if (!wglMakeCurrent(NULL, NULL))
+            serrorBox("Release of DC and RC failed.");
+        if (!wglDeleteContext(hRC))
+            serrorBox("Release rendering context failed.");
+    }
+    hRC = NULL;
+
+    if (hDC && !ReleaseDC(hWnd, hDC))
+        serrorBox("Release device context failed.");
+    hDC = NULL;
+
+    if (hWnd && !DestroyWindow(hWnd))
+        serrorBox("Could not release window.");
+    hWnd = NULL;
+
+    if (!UnregisterClass("OpenGL", hInstance))
+        serrorBox("Could not unregister class");
+    hInstance = NULL;
+}
+
+BOOL createWindow(const char *title, int width, int height)
+{
+    GLuint pixelFormat;
+    WNDCLASS wc;
+    DWORD dwExStyle;
+    DWORD dwStyle;
+    RECT windowRect;
+
+    windowRect.left = 0;
+    windowRect.right = width;
+    windowRect.top = 0;
+    windowRect.bottom = height;
+
+    hInstance = GetModuleHandle(NULL);
+    wc.style = CS_HREDRAW | CS_VREDRAW | CS_OWNDC;
+    wc.lpfnWndProc = (WNDPROC) wndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = NULL;
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "OpenGL";
+
+    if (!RegisterClass(&wc)) {
+        errorBox("Failed to register window class.");
+        return FALSE;
+    }
+
+    dwExStyle = WS_EX_APPWINDOW | WS_EX_WINDOWEDGE;
+    dwStyle = WS_OVERLAPPEDWINDOW;
+    dwStyle |= WS_CLIPSIBLINGS | WS_CLIPCHILDREN;
+
+    AdjustWindowRectEx(&windowRect, dwStyle, FALSE, dwExStyle);
+    hWnd = CreateWindowEx(dwExStyle, "OpenGL", title, dwStyle, 0, 0,
+                          windowRect.right - windowRect.left,
+                          windowRect.bottom - windowRect.top,
+                          NULL, NULL, hInstance, NULL);
+    if (!hWnd) {
+        killGLWindow();
+        errorBox("Window creation error.");
+        return FALSE;
+    }
+
+    static PIXELFORMATDESCRIPTOR pfd = {
+        sizeof(PIXELFORMATDESCRIPTOR),
+        1,
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        PFD_TYPE_RGBA,
+        32,
+        0, 0, 0, 0, 0, 0, // color bits
+        0, // alpha
+        0, // shift
+        0, 0, 0, 0, 0, // accumulation
+        16, // depth buffer
+        0, // stencil
+        0, // auxiliary
+        PFD_MAIN_PLANE, // drawing layer
+        0, //reserved
+        0, 0, 0 // layer masks
+    };
+
+    hDC = GetDC(hWnd);
+    if (!hDC) {
+        killGLWindow();
+        errorBox("Can't create OpenGL device context.");
+        return FALSE;
+    }
+
+    pixelFormat = ChoosePixelFormat(hDC, &pfd);
+    if (!pixelFormat) {
+        killGLWindow();
+        errorBox("Can't find a suitable pixel format.");
+        return FALSE;
+    }
+
+    if (!SetPixelFormat(hDC, pixelFormat, &pfd)) {
+        killGLWindow();
+        errorBox("Can't set pixel format.");
+        return FALSE;
+    }
+
+    hRC = wglCreateContext(hDC);
+    if (!hRC) {
+        killGLWindow();
+        errorBox("Can't create OpenGL rendering context.");
+        return FALSE;
+    }
+
+    if (!wglMakeCurrent(hDC, hRC)) {
+        killGLWindow();
+        errorBox("Can't activate OpenGL rendering context.");
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, SW_SHOW);
+    SetForegroundWindow(hWnd);
+    SetFocus(hWnd);
+    handleResize(width, height);
+
+    return TRUE;
+}
+
+static LRESULT CALLBACK wndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg) {
+    case WM_ACTIVATE:
+        inactive = HIWORD(wParam) != 0;
+        return 0;
+
+    case WM_SYSCOMMAND:
+        switch (wParam) {
+        case SC_SCREENSAVE:
+        case SC_MONITORPOWER:
+            return 0;
+        }
+        break;
+
+    case WM_CLOSE:
+        PostQuitMessage(0);
+        return 0;
+
+    case WM_KEYDOWN:
+        // Handle key down
+        return 0;
+
+    case WM_KEYUP:
+        // handle key up
+        return 0;
+
+    case WM_SIZE:
+        handleResize(LOWORD(lParam), HIWORD(lParam));
+        return 0;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int iCmdShow)
+{
+    MSG msg;
+    BOOL done = FALSE;
+
+    if (!createWindow("Game", 768, 480))
+        return 0;
+
+    while (!done) {
+        if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+            if (msg.message == WM_QUIT) {
+                done = TRUE;
+            } else {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+        } else {
+            if (!inactive) {
+                handleRedraw();
+                SwapBuffers(hDC);
+            }
+        }
+    }
+
+    killGLWindow();
+    return 0;
+}
