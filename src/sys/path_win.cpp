32bca1ca03e668b2352a032faf1ff28795704c1c
Dietrich Epp <depp@zdome.net>
2011-12-16 00:50:17 -0800

    Add placeholder Windows path code

---
+++ src/sys/path_win.cpp
@@ -0,0 +1,15 @@
+#include "path.hpp"
+
+void Path::init()
+{
+    // FIXME: unimplemented
+}
+
+#include <stdlib.h>
+
+IFile *Path::openIFile(std::string const &path)
+{
+    // FIXME: unimplemented
+    abort();
+    return NULL;
+}
