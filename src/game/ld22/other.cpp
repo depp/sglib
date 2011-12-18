2866543eeca4fefea005d523d93bc2ed30113458
Dietrich Epp <depp@zdome.net>
2011-12-18 01:33:42 -0800

    Make Other chase after items

--- src/game/ld22/other.cpp
+++ src/game/ld22/other.cpp
@@ -1,4 +1,5 @@
 #include "other.hpp"
+#include "item.hpp"
 using namespace LD22;
 
 Other::~Other()
@@ -6,5 +7,20 @@ Other::~Other()
 
 void Other::advance()
 {
+    scanItems();
+    if (m_item) {
+        int wx = centerx(), wy = centery();
+        int ix = m_item->centerx(), iy = m_item->centery();
+        int dx = ix - wx, dy = iy - wy;
+        if (dx < -20)
+            m_xpush = -PUSH_SCALE;
+        else if (dx > 20)
+            m_xpush = PUSH_SCALE;
+        else
+            m_xpush = 0;
+        (void) dy;
+    } else {
+        m_xpush = 0;
+    }
     Walker::advance();
 }
