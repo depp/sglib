97327539393b62eec5cacc0ba0017b37d38971b6
Dietrich Epp <depp@zdome.net>
2012-01-31 15:49:26 -0800

    Add fallback for git describe

--- scripts/gen/git.py
+++ scripts/gen/git.py
@@ -1,7 +1,11 @@
 import gen.shell as shell
+import subprocess
 
 def describe(obj, path):
-    """Call 'git-describe' on the given path."""
+    """Call 'git-describe' on the given, or fall back to SHA-1."""
     git = obj.opts.git_path or 'git'
-    desc = shell.getoutput([git, 'describe'], cwd=path)
+    try:
+        desc = shell.getoutput([git, 'describe'], cwd=path)
+    except subprocess.CalledProcessError:
+        desc = shell.getoutput([git, 'rev-parse', 'HEAD'], cwd=path)
     return desc.strip()
