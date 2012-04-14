8c8fbeec4cf8ac718ca6571187974329b7d619b1
Dietrich Epp <depp@zdome.net>
2012-04-14 15:46:01 -0700

    Pull upstream changes
    There are some changes to SGLib, which have resulted in a new build script and
    git ignore file.

---
+++ init.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import os
+import sys
+root = sys.path[0]
+sglib = os.path.join(root, 'sglib')
+sys.path.insert(1, os.path.join(sglib, 'scripts'))
+from gen.project import *
+p = Project(root)
+
+p.info.set(
+    PKG_NAME = 'SGLib Tests',
+    PKG_IDENT = 'us.moria.img-test',
+    PKG_EMAIL = 'depp@zdome.net',
+    PKG_URL = 'http://moria.us/',
+    PKG_COPYRIGHT = u'Copyright \xa9 2011-2012 Dietrich Epp',
+    DEFAULT_CVARS = [
+        ('log.level.root', 'debug'),
+    ]
+)
+
+p.add_module(Executable(
+    'IMAGE', 'Image Test executable',
+    reqs = 'SGLIB',
+    EXE_NAME = 'Image Test',
+    DEFAULT_CVARS = [
+        ('path.data-path', Path('test/image/data')),
+    ]
+))
+
+p.add_sourcelist_str('SGLib Tests', '.', """\
+test/image/image.c
+""", 'IMAGE')
+
+p.run()
