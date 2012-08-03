7c895f42331b50b50aad5e900195a56c88c1e42d
Dietrich Epp <depp@zdome.net>
2012-08-03 13:30:00 -0700

    audio: stabilize ALSA timestamps

--- src/base/audio_alsa.c
+++ src/base/audio_alsa.c
@@ -2,6 +2,7 @@
 
 #include "audio.h"
 #include "clock.h"
+#include "clock_impl.h"
 #include "cvar.h"
 #include "error.h"
 #include "log.h"
@@ -16,14 +17,15 @@
 #include <unistd.h>
 
 struct sg_audio_alsa {
-    const char *name;
-    int rate;
-    int bufsize;
+    const char *req_name;
+    int req_rate;
+    int req_bufsize;
 
     snd_pcm_t *pcm;
     struct sg_audio_system *sys;
     float *buffer;
-    int realrate;
+    int rate;
+    int bufsize;
     int periodsize;
 };
 
@@ -32,17 +34,44 @@ static struct sg_logger *sg_audio_alsa_logger;
 static void
 sg_audio_alsa_getinfo(struct sg_audio_alsa *alsa)
 {
-    if (!sg_cvar_gets("audio", "alsadevice", &alsa->name))
-        alsa->name = "default";
-    if (!sg_cvar_geti("audio", "rate", &alsa->rate))
-        alsa->rate = 48000;
-    assert(alsa->rate > 0);
-    if (!sg_cvar_geti("audio", "bufsize", &alsa->bufsize))
-        alsa->bufsize = 1024;
-    assert(alsa->bufsize > 0);
+    if (!sg_cvar_gets("audio", "alsadevice", &alsa->req_name))
+        alsa->req_name = "default";
+    if (!sg_cvar_geti("audio", "rate", &alsa->req_rate))
+        alsa->req_rate = 48000;
+    assert(alsa->req_rate > 0);
+    if (!sg_cvar_geti("audio", "bufsize", &alsa->req_bufsize))
+        alsa->req_bufsize = 1024;
+    assert(alsa->req_bufsize > 0);
 }
 
+/*
+  ALSA timestamps:
 
+  - Retrieve the status with 'snd_pcm_status'
+  - Get "now" timestamp with 'snd_pcm_status_get_htstamp'
+  - Get number of frames available with 'snd_pcm_status_get_avail'
+
+  The "now" timestamp is the timestamp of the frame currently being
+  read, so to get the timestamp of the frame currently being written
+  we have to add the length of sound currently in the buffer.  This is
+  calculated by subtracting available frames from the buffer size, and
+  then dividing by the sample rate.
+
+  Tests indicate that the resulting timestamps are relatively stable,
+  and unaffected by any additional calls to 'usleep' in the callback
+  function.
+
+  The ALSA-supplied timestamps are not as useful as they could be.
+  They use either the monotonic clock or the realtime clock, but there
+  is no good way to tell which.  Since we just woke up, we simply make
+  a separate call to get the monotonic clock, and use this timestamp
+  to calculate the game tick corresponding to the current write
+  position.
+
+  So we end up calculating the time stamp of the first sample in the
+  write buffer, and then platform-independent code will manage latency
+  and jitter.
+*/
 static void *
 sg_audio_loop(void *ptr)
 {
@@ -50,11 +79,35 @@ sg_audio_loop(void *ptr)
     struct sg_audio_system *sys = alsa->sys;
     snd_pcm_t *pcm = alsa->pcm;
     float *buf = alsa->buffer;
-    unsigned time, periodsize = alsa->periodsize, pos;
+    unsigned periodsize = alsa->periodsize, bufsize = alsa->bufsize,
+        rate = alsa->rate, time, pos, dx, dt, nsec;
     snd_pcm_sframes_t r;
+    snd_pcm_uframes_t nframes;
+    struct timespec ts;
+    snd_pcm_status_t *status;
+    snd_pcm_status_alloca(&status);
 
     while (1) {
-        time = sg_clock_get();
+        snd_pcm_wait(pcm, 1000);
+
+        r = snd_pcm_status(pcm, status);
+        if (r < 0) {
+            sg_logf(sg_audio_alsa_logger, LOG_INFO,
+                    "snd_pcm_status: %s", snd_strerror(r));
+            goto done;
+        }
+        nframes = snd_pcm_status_get_avail(status);
+        clock_gettime(CLOCK_MONOTONIC, &ts);
+
+        dx = bufsize - nframes;
+        /* This will overflow if latency is more than a couple
+           seconds.  If latency is that bad, we don't care.  */
+        dt = (unsigned) ((1000000000ULL * dx) / rate);
+        nsec = ts.tv_nsec + dt;
+        ts.tv_sec += nsec / 1000000000;
+        ts.tv_nsec = nsec % 1000000000;
+        time = sg_clock_convert(&ts);
+
         sg_audio_system_pull(sys, time, buf, periodsize);
         pos = 0;
         while (pos < periodsize) {
@@ -105,6 +158,7 @@ sg_audio_initsys(void)
     struct sg_error *err = NULL;
     struct sg_audio_alsa *alsa;
     snd_pcm_hw_params_t *hwparms = NULL;
+    snd_pcm_sw_params_t *swparms = NULL;
     pthread_t thread;
     pthread_attr_t threadattr;
     const char *why;
@@ -125,13 +179,15 @@ sg_audio_initsys(void)
     sg_audio_alsa_getinfo(alsa);
     r = snd_pcm_open(
         &alsa->pcm,
-        alsa->name,
+        alsa->req_name,
         SND_PCM_STREAM_PLAYBACK,
         0);
     if (r < 0) {
         why = "could not open audio device";
         goto alsa_error;
     }
+
+    /* ==================== */
 
     snd_pcm_hw_params_alloca(&hwparms);
     r = snd_pcm_hw_params_any(alsa->pcm, hwparms);
@@ -171,7 +227,7 @@ sg_audio_initsys(void)
         goto alsa_error;
     }
 
-    rate = alsa->rate;
+    rate = alsa->req_rate;
     r = snd_pcm_hw_params_set_rate_near(
         alsa->pcm, hwparms, &rate, 0);
     if (r < 0) {
@@ -185,9 +241,9 @@ sg_audio_initsys(void)
                 "audio sample rate too extreme: %u", rate);
         goto cleanup;
     }
-    alsa->realrate = rate;
+    alsa->rate = rate;
 
-    bufsize = alsa->bufsize;
+    bufsize = alsa->req_bufsize;
     r = snd_pcm_hw_params_set_buffer_size_near(
         alsa->pcm, hwparms, &bufsize);
     if (r < 0) {
@@ -196,6 +252,7 @@ sg_audio_initsys(void)
     }
     sg_logf(sg_audio_alsa_logger, LOG_INFO,
             "audio buffer size: %lu", bufsize);
+    alsa->bufsize = bufsize;
 
     r = snd_pcm_hw_params_get_period_size(
         hwparms, &periodsize, 0);
@@ -213,6 +270,26 @@ sg_audio_initsys(void)
         why = "could not set hardware parameters";
         goto alsa_error;
     }
+
+    /* ==================== */
+
+    snd_pcm_sw_params_alloca(&swparms);
+    r = snd_pcm_sw_params_current(alsa->pcm, swparms);
+    if (r < 0) {
+        why = "could not get software parameters";
+        goto alsa_error;
+    }
+
+    snd_pcm_uframes_t avail_min;
+    r = snd_pcm_sw_params_get_avail_min(swparms, &avail_min);
+    if (r < 0) {
+        why = "could not get avail_min";
+        goto alsa_error;
+    }
+    sg_logf(sg_audio_alsa_logger, LOG_INFO,
+            "audio buffer threshold: %u", (unsigned) avail_min);
+
+    /* ==================== */
 
     alsa->sys = sg_audio_system_new(rate, &err);
     if (!alsa->sys) {
