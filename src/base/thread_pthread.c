02138b0a0fe4db5fdaaad555d6bbe1041df56c07
Dietrich Epp <depp@zdome.net>
2012-08-18 20:47:57 -0700

    threading: add RW lock type

--- src/base/thread_pthread.c
+++ src/base/thread_pthread.c
@@ -71,3 +71,105 @@ sg_lock_release(struct sg_lock *p)
 err:
     abort();
 }
+
+/* ======================================== */
+
+void
+sg_rwlock_init(struct sg_rwlock *p)
+{
+    pthread_rwlockattr_t lattr;
+    int r;
+    r = pthread_rwlockattr_init(&lattr);
+    if (r) goto err;
+    r = pthread_rwlockattr_setpshared(&lattr, PTHREAD_PROCESS_PRIVATE);
+    if (r) goto err;
+    r = pthread_rwlock_init(&p->l, &lattr);
+    if (r) goto err;
+    r = pthread_rwlockattr_destroy(&lattr);
+    if (r) goto err;
+    return;
+
+err:
+    abort();
+
+}
+
+void
+sg_rwlock_destroy(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_destroy(&p->l);
+    if (r) goto err;
+    return;
+
+err:
+    abort();
+}
+
+void
+sg_rwlock_wracquire(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_wrlock(&p->l);
+    if (r) goto err;
+    return;
+
+err:
+    abort();
+}
+
+int
+sg_rwlock_wrtry(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_trywrlock(&p->l);
+    if (r == EBUSY)
+        return 0;
+    if (r == 0)
+        return 1;
+
+    abort();
+}
+
+void
+sg_rwlock_wrrelease(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_unlock(&p->l);
+    if (r) goto err;
+    return;
+
+err:
+    abort();
+}
+
+void
+sg_rwlock_rdacquire(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_rdlock(&p->l);
+    if (r) goto err;
+    return;
+
+err:
+    abort();
+}
+
+int
+sg_rwlock_rdtry(struct sg_rwlock *p)
+{
+    int r;
+    r = pthread_rwlock_tryrdlock(&p->l);
+    if (r == EBUSY)
+        return 0;
+    if (r == 0)
+        return 1;
+
+    abort();
+}
+
+void
+sg_rwlock_rdrelease(struct sg_rwlock *p)
+{
+    sg_rwlock_wrrelease(p);
+}
