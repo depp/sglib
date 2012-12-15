c7c38302cd73b65a2367b8c56d87ab8498bd90e4
Dietrich Epp <depp@zdome.net>
2012-12-15 00:54:40 -0800

    Remove GLU dependency

--- sg/core/opengl.c
+++ sg/core/opengl.c
@@ -4,9 +4,31 @@
 
 const struct sg_error_domain SG_ERROR_OPENGL = { "opengl" };
 
+static const struct {
+    int code;
+    const char *name;
+} SG_ERROR_OPENGL_NAMES[] = {
+    { 0x0500, "INVALID_ENUM​" },
+    { 0x0501, "INVALID_VALUE​" },
+    { 0x0502, "INVALID_OPERATION​" },
+    { 0x0503, "OUT_OF_MEMORY​" },
+    { 0x0506, "INVALID_FRAMEBUFFER_OPERATION​" },
+    { 0x0503, "STACK_OVERFLOW​" },
+    { 0x0504, "STACK_UNDERFLOW​" },
+    { 0x8031, "TABLE_TOO_LARGE​1" },
+    { 0, 0 }
+};
+
 void
 sg_error_opengl(struct sg_error **err, GLenum code)
 {
-    sg_error_sets(err, &SG_ERROR_OPENGL, code,
-                  (char *) gluErrorString(code));
+    int i;
+    const char *name = NULL;
+    for (i = 0; SG_ERROR_OPENGL_NAMES[i].code; i++) {
+        if (SG_ERROR_OPENGL_NAMES[i].code == code) {
+            name = SG_ERROR_OPENGL_NAMES[i].name;
+            break;
+        }
+    }
+    sg_error_sets(err, &SG_ERROR_OPENGL, code, name);
 }
