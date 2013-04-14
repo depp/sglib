depp/sglib

commit fb4b1dd9a04bb0c7d96e1702f9bbc404432a9bbd

reorg: update names and include paths

include/sg/entry.h
@@ -1,13 +1,14 @@
-#ifndef BASE_ENTRY_H
-#define BASE_ENTRY_H
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
+#ifndef SG_ENTRY_H
+#define SG_ENTRY_H
 /* Entry points: the layer that connects library code, game code, and
    platform code.  */
 #include <stdarg.h>
-#include "defs.h"
+#include "sg/defs.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
-union sg_event;
+union pce_event;
 struct sg_error;
 struct sg_logger;
 
@@ -43,7 +44,7 @@ sg_game_getinfo(struct sg_game_info *info);
 /* Called for every event.  The OpenGL context may not be valid, you
    should not attempt to draw to the screen, load textures, etc.  */
 void
-sg_game_event(union sg_event *evt);
+sg_game_event(union pce_event *evt);
 
 /* Draw the scene.  The OpenGL context will be valid.  The x, y,
    width, and height parameters specify the current viewport, the game
@@ -90,7 +91,7 @@ sg_sys_getinfo(struct sg_game_info *info);
 
 /* Called for every event.  */
 void
-sg_sys_event(union sg_event *evt);
+sg_sys_event(union pce_event *evt);
 
 /* Draw the scene.  The OpenGL context must be valid.  */
 void

include/sg/event.h
@@ -1,25 +1,26 @@
-#ifndef BASE_EVENT_H
-#define BASE_EVENT_H
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
+#ifndef SG_EVENT_H
+#define SG_EVENT_H
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-    /* sg_event_mouse */
+    /* pce_event_mouse */
     SG_EVENT_MDOWN,
     SG_EVENT_MUP,
     SG_EVENT_MMOVE,
 
-    /* sg_event_key */
+    /* pce_event_key */
     SG_EVENT_KDOWN,
     SG_EVENT_KUP,
 
-    /* sg_event_resize */
+    /* pce_event_resize */
     SG_EVENT_RESIZE,
 
-    /* sg_event_status */
+    /* pce_event_status */
     SG_EVENT_STATUS
-} sg_event_type_t;
+} pce_event_type_t;
 
 /* Mouse buttons */
 enum {
@@ -35,33 +36,33 @@ enum {
     SG_STATUS_FULLSCREEN = 02
 };
 
-struct sg_event_mouse {
-    sg_event_type_t type;
+struct pce_event_mouse {
+    pce_event_type_t type;
     int button; /* Always -1 for MMOVE */
     int x, y;
 };
 
-struct sg_event_key {
-    sg_event_type_t type;
+struct pce_event_key {
+    pce_event_type_t type;
     int key;
 };
 
-struct sg_event_resize {
-    sg_event_type_t type;
+struct pce_event_resize {
+    pce_event_type_t type;
     int width, height;
 };
 
-struct sg_event_status {
-    sg_event_type_t type;
+struct pce_event_status {
+    pce_event_type_t type;
     unsigned status;
 };
 
-union sg_event {
-    sg_event_type_t type;
-    struct sg_event_mouse mouse;
-    struct sg_event_key key;
-    struct sg_event_resize resize;
-    struct sg_event_status status;
+union pce_event {
+    pce_event_type_t type;
+    struct pce_event_mouse mouse;
+    struct pce_event_key key;
+    struct pce_event_resize resize;
+    struct pce_event_status status;
 };
 
 #ifdef __cplusplus

sg/core/sys.c
@@ -1,15 +1,16 @@
-#include "audio_system.h"
-#include "clock.h"
-#include "dispatch.h"
-#include "entry.h"
-#include "event.h"
-#include "file.h"
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 #include "keycode/keycode.h"
-#include "log.h"
-#include "rand.h"
-#include "record.h"
-#include "resource.h"
-#include "version.h"
+#include "sg/audio_system.h"
+#include "sg/clock.h"
+#include "sg/dispatch.h"
+#include "sg/entry.h"
+#include "sg/event.h"
+#include "sg/file.h"
+#include "sg/log.h"
+#include "sg/rand.h"
+#include "sg/record.h"
+#include "sg/resource.h"
+#include "sg/version.h"
 
 struct sg_sys_state sg_sst;
 static struct sg_logger *sg_log_video;
@@ -76,7 +77,7 @@ sg_sys_getinfo(struct sg_game_info *info)
 }
 
 void
-sg_sys_event(union sg_event *evt)
+sg_sys_event(union pce_event *evt)
 {
     const char *status;
     switch (evt->type) {

sg/gtk/gtk.c
@@ -1,18 +1,19 @@
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 #if defined(__GNUC__) && \
     ((__GNUC__ == 4 && __GNUC_MINOR >= 6) || __GNUC__ > 4)
 #define HAVE_DPUSH 1
 #endif
 
-#include "base/audio_system.h"
-#include "base/clock.h"
-#include "base/cvar.h"
-#include "base/entry.h"
-#include "base/error.h"
-#include "base/event.h"
-#include "base/keycode/keycode.h"
-#include "base/keycode/keytable.h"
-#include "base/opengl.h"
-#include "base/version.h"
+#include "keycode/keycode.h"
+#include "keycode/keytable.h"
+#include "sg/audio_system.h"
+#include "sg/clock.h"
+#include "sg/cvar.h"
+#include "sg/entry.h"
+#include "sg/error.h"
+#include "sg/event.h"
+#include "sg/opengl.h"
+#include "sg/version.h"
 
 /* The Gtk headers generate a warning.  */
 #if defined(HAVE_DPUSH)
@@ -117,7 +118,7 @@ handle_destroy(GtkWidget *widget, GdkEvent *event, void *user_data)
 static gboolean
 handle_expose(GtkWidget *area)
 {
-    struct sg_event_resize rsz;
+    struct pce_event_resize rsz;
     GtkAllocation a;
     GdkGLContext *context = gtk_widget_get_gl_context(area);
     GdkGLDrawable *drawable = gtk_widget_get_gl_drawable(area);
@@ -146,7 +147,7 @@ handle_expose(GtkWidget *area)
             rsz.type = SG_EVENT_RESIZE;
             rsz.width = a.width;
             rsz.height = a.height;
-            sg_sys_event((union sg_event *) &rsz);
+            sg_sys_event((union pce_event *) &rsz);
         }
     }
     sg_sys_draw();
@@ -176,10 +177,10 @@ update(void *obj)
 }
 
 static gboolean
-handle_key(GdkEventKey *e, sg_event_type_t t)
+handle_key(GdkEventKey *e, pce_event_type_t t)
 {
     int code = e->hardware_keycode, hcode;
-    struct sg_event_key evt;
+    struct pce_event_key evt;
     if (e->keyval == GDK_F11) {
         if (e->type == GDK_KEY_PRESS)
             toggle_fullscreen();
@@ -192,26 +193,26 @@ handle_key(GdkEventKey *e, sg_event_type_t t)
         return TRUE;
     evt.type = t;
     evt.key = hcode;
-    sg_sys_event((union sg_event *) &evt);
+    sg_sys_event((union pce_event *) &evt);
     return TRUE;
 }
 
 static gboolean
 handle_motion(GdkEventMotion *e)
 {
-    struct sg_event_mouse evt;
+    struct pce_event_mouse evt;
     evt.type = SG_EVENT_MMOVE;
     evt.button = -1;
     evt.x = e->x;
     evt.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union sg_event *) &evt);
+    sg_sys_event((union pce_event *) &evt);
     return TRUE;
 }
 
 static gboolean
-handle_button(GdkEventButton *e, sg_event_type_t t)
+handle_button(GdkEventButton *e, pce_event_type_t t)
 {
-    struct sg_event_mouse evt;
+    struct pce_event_mouse evt;
     evt.type = t;
     switch (e->button) {
     case 1:  evt.button = SG_BUTTON_LEFT; break;
@@ -221,7 +222,7 @@ handle_button(GdkEventButton *e, sg_event_type_t t)
     }
     evt.x = e->x;
     evt.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union sg_event *) &evt);
+    sg_sys_event((union pce_event *) &evt);
     return TRUE;
 }
 
@@ -269,7 +270,7 @@ handle_window_state(GtkWidget *widget, GdkEvent *event, gpointer user_data)
     GdkWindowState st = event->window_state.new_window_state;
     int fullscreen, shown;
     unsigned status;
-    struct sg_event_status evt;
+    struct pce_event_status evt;
     (void) user_data;
     (void) widget;
     print_wstate_flags(st, event->window_state.changed_mask);
@@ -284,7 +285,7 @@ handle_window_state(GtkWidget *widget, GdkEvent *event, gpointer user_data)
     }
     evt.type = SG_EVENT_STATUS;
     evt.status = status;
-    sg_sys_event((union sg_event *) &evt);
+    sg_sys_event((union pce_event *) &evt);
     sg_gtk_status = status;
     return TRUE;
 }

sg/osx/GDisplay.h
@@ -1,3 +1,4 @@
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 // The GDisplay object is the center of the Mac UI code: it manages a window from the viewpoint of the common code.  It sends Cocoa events to the common code, and changes the window configuration in response to calls from the common code.
 // The display will also manage the OpenGL context and rendering thread / rendering timer.
 // Each display has a lock for accessing the OpenGL context and window object.  There is no need to obtain a lock, all calls are safe to call from the main thread and -update is safe to call from any thread.
@@ -50,11 +51,11 @@ typedef enum {
 - (IBAction)showFullScreen:(id)sender;
 - (IBAction)toggleGFullScreen:(id)sender;
 
-- (void)handleUIEvent:(union sg_event *)event;
+- (void)handleUIEvent:(union pce_event *)event;
 
 // Safe to call from any thread (all other methods must be called from main thread)
 - (void)update;
 
 @end
 
-void GDisplayKeyEvent(GDisplay *w, NSEvent *e, sg_event_type_t t);
+void GDisplayKeyEvent(GDisplay *w, NSEvent *e, pce_event_type_t t);

sg/osx/GDisplay.m
@@ -1,3 +1,4 @@
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 #import "base/opengl.h"
 #import <Carbon/Carbon.h>
 #import "GDisplay.h"
@@ -33,13 +34,13 @@ static bool isWindowedMode(GDisplayMode mode)
 
 /* Event handling */
 
-void GDisplayKeyEvent(GDisplay *w, NSEvent *e, sg_event_type_t t)
+void GDisplayKeyEvent(GDisplay *w, NSEvent *e, pce_event_type_t t)
 {
     int ncode = MAC_NATIVE_TO_HID[[e keyCode] & 0x7F];
-    struct sg_event_key evt;
+    struct pce_event_key evt;
     evt.type = t;
     evt.key = ncode;
-    [w handleUIEvent:(union sg_event *) &evt];
+    [w handleUIEvent:(union pce_event *) &evt];
 }
 
 /* Rendering callback */
@@ -60,7 +61,7 @@ static CVReturn cvCallback(CVDisplayLinkRef displayLink, const CVTimeStamp *now,
 
 /* Private methods */
 
-static void handleMouse(GDisplay *d, NSEvent *e, sg_event_type_t t, int button)
+static void handleMouse(GDisplay *d, NSEvent *e, pce_event_type_t t, int button)
 {
     // FIXME need to translate if there are multiple screens
     if ([e window]) {
@@ -68,12 +69,12 @@ static void handleMouse(GDisplay *d, NSEvent *e, sg_event_type_t t, int button)
         return;
     }
     NSPoint pt = [e locationInWindow];
-    struct sg_event_mouse evt;
+    struct pce_event_mouse evt;
     evt.type = t;
     evt.button = button;
     evt.x = pt.x;
     evt.y = pt.y;
-    [d handleUIEvent:(union sg_event *) &evt];
+    [d handleUIEvent:(union pce_event *) &evt];
 }
 
 @implementation GDisplay (Private)
@@ -217,7 +218,7 @@ static void handleMouse(GDisplay *d, NSEvent *e, sg_event_type_t t, int button)
 
 - (void)stateChanged {
     unsigned status;
-    struct sg_event_status evt;
+    struct pce_event_status evt;
     if ([NSApp isHidden]) {
         status = 0;
     } else if (mode_ == GDisplayWindow) {
@@ -230,7 +231,7 @@ static void handleMouse(GDisplay *d, NSEvent *e, sg_event_type_t t, int button)
     }
     evt.type = SG_EVENT_STATUS;
     evt.status = status;
-    sg_sys_event((union sg_event *) &evt);
+    sg_sys_event((union pce_event *) &evt);
 }
 
 @end
@@ -390,7 +391,7 @@ error:
         [self showWindow:sender];
 }
 
-- (void)handleUIEvent:(union sg_event *)event {
+- (void)handleUIEvent:(union pce_event *)event {
     [self lock];
     sg_sys_event(event);
     [self unlock];
@@ -407,11 +408,11 @@ error:
     if (frameChanged_) {
         [context_ update];
         glViewport(0, 0, width_, height_);
-        struct sg_event_resize evt;
+        struct pce_event_resize evt;
         evt.type = SG_EVENT_RESIZE;
         evt.width = width_;
         evt.height = height_;
-        sg_sys_event((union sg_event *) &evt);
+        sg_sys_event((union pce_event *) &evt);
     }
     sg_sys_draw();
     [context_ flushBuffer];

sg/osx/GView.m
@@ -1,15 +1,16 @@
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 #import "GView.h"
 #import "GDisplay.h"
 
-static void handleMouse(GView *v, NSEvent *e, sg_event_type_t t, int button)
+static void handleMouse(GView *v, NSEvent *e, pce_event_type_t t, int button)
 {
     NSPoint pt = [v convertPoint:[e locationInWindow] fromView:nil];
-    struct sg_event_mouse evt;
+    struct pce_event_mouse evt;
     evt.type = t;
     evt.button = button;
     evt.x = pt.x;
     evt.y = pt.y;
-    [v->display_ handleUIEvent:(union sg_event *) &evt];
+    [v->display_ handleUIEvent:(union pce_event *) &evt];
 }
 
 @implementation GView

sg/sdl/sdl.c
@@ -1,3 +1,4 @@
+/* Copyright 2012 Dietrich Epp <depp@zdome.net> */
 #include "base/cvar.h"
 #include "base/entry.h"
 #include "base/error.h"
@@ -80,7 +81,7 @@ static SDL_Surface *sdl_surface;
 static void
 init(int argc, char *argv[])
 {
-    struct sg_event_resize rsz;
+    struct pce_event_resize rsz;
     struct sg_game_info gameinfo;
     int opt;
 
@@ -111,24 +112,24 @@ init(int argc, char *argv[])
     rsz.type = SG_EVENT_RESIZE;
     rsz.width = sdl_surface->w;
     rsz.height = sdl_surface->h;
-    sg_sys_event((union sg_event *) &rsz);
+    sg_sys_event((union pce_event *) &rsz);
 }
 
 static void
 sdl_event_mousemove(SDL_MouseMotionEvent *e)
 {
-    struct sg_event_mouse ee;
+    struct pce_event_mouse ee;
     ee.type = SG_EVENT_MMOVE;
     ee.button = -1;
     ee.x = e->x;
     ee.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union sg_event *) &ee);
+    sg_sys_event((union pce_event *) &ee);
 }
 
 static void
 sdl_event_mousebutton(SDL_MouseButtonEvent *e)
 {
-    struct sg_event_mouse ee;
+    struct pce_event_mouse ee;
     ee.type = e->type == SDL_MOUSEBUTTONDOWN ?
         SG_EVENT_MDOWN : SG_EVENT_MUP;
     switch (e->button) {
@@ -141,29 +142,29 @@ sdl_event_mousebutton(SDL_MouseButtonEvent *e)
     }
     ee.x = e->x;
     ee.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union sg_event *) &ee);
+    sg_sys_event((union pce_event *) &ee);
 }
 
 static void
 sdl_event_key(SDL_KeyboardEvent *e)
 {
-    struct sg_event_key ee;
+    struct pce_event_key ee;
     ee.type = e->type == SDL_KEYDOWN ?
         SG_EVENT_KDOWN : SG_EVENT_KUP;
     ee.key = -1;
-    sg_sys_event((union sg_event *) &ee);
+    sg_sys_event((union pce_event *) &ee);
 }
 
 static void
 sdl_event_resize(SDL_ResizeEvent *e)
 {
-    struct sg_event_resize ee;
+    struct pce_event_resize ee;
     sg_window_width = e->w;
     sg_window_height = e->h;
     ee.type = SG_EVENT_RESIZE;
     ee.width = e->w;
     ee.height = e->h;
-    sg_sys_event((union sg_event *) &ee);
+    sg_sys_event((union pce_event *) &ee);
 }
 
 static void

commit 6231d2a9d01a90abd226b62434c19e646d8bdc45

Fix overzealous search and replace
The error was introduced in fb4b1dd9a04bb0c7d96e1702f9bbc404432a9bbd.

include/sg/entry.h
@@ -8,7 +8,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-union pce_event;
+union sg_event;
 struct sg_error;
 struct sg_logger;
 
@@ -44,7 +44,7 @@ sg_game_getinfo(struct sg_game_info *info);
 /* Called for every event.  The OpenGL context may not be valid, you
    should not attempt to draw to the screen, load textures, etc.  */
 void
-sg_game_event(union pce_event *evt);
+sg_game_event(union sg_event *evt);
 
 /* Draw the scene.  The OpenGL context will be valid.  The x, y,
    width, and height parameters specify the current viewport, the game
@@ -91,7 +91,7 @@ sg_sys_getinfo(struct sg_game_info *info);
 
 /* Called for every event.  */
 void
-sg_sys_event(union pce_event *evt);
+sg_sys_event(union sg_event *evt);
 
 /* Draw the scene.  The OpenGL context must be valid.  */
 void

include/sg/event.h
@@ -6,21 +6,21 @@ extern "C" {
 #endif
 
 typedef enum {
-    /* pce_event_mouse */
+    /* sg_event_mouse */
     SG_EVENT_MDOWN,
     SG_EVENT_MUP,
     SG_EVENT_MMOVE,
 
-    /* pce_event_key */
+    /* sg_event_key */
     SG_EVENT_KDOWN,
     SG_EVENT_KUP,
 
-    /* pce_event_resize */
+    /* sg_event_resize */
     SG_EVENT_RESIZE,
 
-    /* pce_event_status */
+    /* sg_event_status */
     SG_EVENT_STATUS
-} pce_event_type_t;
+} sg_event_type_t;
 
 /* Mouse buttons */
 enum {
@@ -36,33 +36,33 @@ enum {
     SG_STATUS_FULLSCREEN = 02
 };
 
-struct pce_event_mouse {
-    pce_event_type_t type;
+struct sg_event_mouse {
+    sg_event_type_t type;
     int button; /* Always -1 for MMOVE */
     int x, y;
 };
 
-struct pce_event_key {
-    pce_event_type_t type;
+struct sg_event_key {
+    sg_event_type_t type;
     int key;
 };
 
-struct pce_event_resize {
-    pce_event_type_t type;
+struct sg_event_resize {
+    sg_event_type_t type;
     int width, height;
 };
 
-struct pce_event_status {
-    pce_event_type_t type;
+struct sg_event_status {
+    sg_event_type_t type;
     unsigned status;
 };
 
-union pce_event {
-    pce_event_type_t type;
-    struct pce_event_mouse mouse;
-    struct pce_event_key key;
-    struct pce_event_resize resize;
-    struct pce_event_status status;
+union sg_event {
+    sg_event_type_t type;
+    struct sg_event_mouse mouse;
+    struct sg_event_key key;
+    struct sg_event_resize resize;
+    struct sg_event_status status;
 };
 
 #ifdef __cplusplus

sg/core/sys.c
@@ -82,7 +82,7 @@ sg_sys_getinfo(struct sg_game_info *info)
 }
 
 void
-sg_sys_event(union pce_event *evt)
+sg_sys_event(union sg_event *evt)
 {
     const char *status;
     switch (evt->type) {

sg/gtk/gtk.c
@@ -118,7 +118,7 @@ handle_destroy(GtkWidget *widget, GdkEvent *event, void *user_data)
 static gboolean
 handle_expose(GtkWidget *area)
 {
-    struct pce_event_resize rsz;
+    struct sg_event_resize rsz;
     GtkAllocation a;
     GdkGLContext *context = gtk_widget_get_gl_context(area);
     GdkGLDrawable *drawable = gtk_widget_get_gl_drawable(area);
@@ -147,7 +147,7 @@ handle_expose(GtkWidget *area)
             rsz.type = SG_EVENT_RESIZE;
             rsz.width = a.width;
             rsz.height = a.height;
-            sg_sys_event((union pce_event *) &rsz);
+            sg_sys_event((union sg_event *) &rsz);
         }
     }
     sg_sys_draw();
@@ -177,10 +177,10 @@ update(void *obj)
 }
 
 static gboolean
-handle_key(GdkEventKey *e, pce_event_type_t t)
+handle_key(GdkEventKey *e, sg_event_type_t t)
 {
     int code = e->hardware_keycode, hcode;
-    struct pce_event_key evt;
+    struct sg_event_key evt;
     if (e->keyval == GDK_F11) {
         if (e->type == GDK_KEY_PRESS)
             toggle_fullscreen();
@@ -193,26 +193,26 @@ handle_key(GdkEventKey *e, pce_event_type_t t)
         return TRUE;
     evt.type = t;
     evt.key = hcode;
-    sg_sys_event((union pce_event *) &evt);
+    sg_sys_event((union sg_event *) &evt);
     return TRUE;
 }
 
 static gboolean
 handle_motion(GdkEventMotion *e)
 {
-    struct pce_event_mouse evt;
+    struct sg_event_mouse evt;
     evt.type = SG_EVENT_MMOVE;
     evt.button = -1;
     evt.x = e->x;
     evt.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union pce_event *) &evt);
+    sg_sys_event((union sg_event *) &evt);
     return TRUE;
 }
 
 static gboolean
-handle_button(GdkEventButton *e, pce_event_type_t t)
+handle_button(GdkEventButton *e, sg_event_type_t t)
 {
-    struct pce_event_mouse evt;
+    struct sg_event_mouse evt;
     evt.type = t;
     switch (e->button) {
     case 1:  evt.button = SG_BUTTON_LEFT; break;
@@ -222,7 +222,7 @@ handle_button(GdkEventButton *e, pce_event_type_t t)
     }
     evt.x = e->x;
     evt.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union pce_event *) &evt);
+    sg_sys_event((union sg_event *) &evt);
     return TRUE;
 }
 
@@ -270,7 +270,7 @@ handle_window_state(GtkWidget *widget, GdkEvent *event, gpointer user_data)
     GdkWindowState st = event->window_state.new_window_state;
     int fullscreen, shown;
     unsigned status;
-    struct pce_event_status evt;
+    struct sg_event_status evt;
     (void) user_data;
     (void) widget;
     print_wstate_flags(st, event->window_state.changed_mask);
@@ -285,7 +285,7 @@ handle_window_state(GtkWidget *widget, GdkEvent *event, gpointer user_data)
     }
     evt.type = SG_EVENT_STATUS;
     evt.status = status;
-    sg_sys_event((union pce_event *) &evt);
+    sg_sys_event((union sg_event *) &evt);
     sg_gtk_status = status;
     return TRUE;
 }

sg/osx/GDisplay.h
@@ -51,11 +51,11 @@ typedef enum {
 - (IBAction)showFullScreen:(id)sender;
 - (IBAction)toggleGFullScreen:(id)sender;
 
-- (void)handleUIEvent:(union pce_event *)event;
+- (void)handleUIEvent:(union sg_event *)event;
 
 // Safe to call from any thread (all other methods must be called from main thread)
 - (void)update;
 
 @end
 
-void GDisplayKeyEvent(GDisplay *w, NSEvent *e, pce_event_type_t t);
+void GDisplayKeyEvent(GDisplay *w, NSEvent *e, sg_event_type_t t);

sg/osx/GDisplay.m
@@ -33,13 +33,13 @@ static bool isWindowedMode(GDisplayMode mode)
 
 /* Event handling */
 
-void GDisplayKeyEvent(GDisplay *w, NSEvent *e, pce_event_type_t t)
+void GDisplayKeyEvent(GDisplay *w, NSEvent *e, sg_event_type_t t)
 {
     int ncode = MAC_NATIVE_TO_HID[[e keyCode] & 0x7F];
-    struct pce_event_key evt;
+    struct sg_event_key evt;
     evt.type = t;
     evt.key = ncode;
-    [w handleUIEvent:(union pce_event *) &evt];
+    [w handleUIEvent:(union sg_event *) &evt];
 }
 
 /* Rendering callback */
@@ -60,7 +60,7 @@ static CVReturn cvCallback(CVDisplayLinkRef displayLink, const CVTimeStamp *now,
 
 /* Private methods */
 
-static void handleMouse(GDisplay *d, NSEvent *e, pce_event_type_t t, int button)
+static void handleMouse(GDisplay *d, NSEvent *e, sg_event_type_t t, int button)
 {
     // FIXME need to translate if there are multiple screens
     if ([e window]) {
@@ -68,12 +68,12 @@ static void handleMouse(GDisplay *d, NSEvent *e, pce_event_type_t t, int button)
         return;
     }
     NSPoint pt = [e locationInWindow];
-    struct pce_event_mouse evt;
+    struct sg_event_mouse evt;
     evt.type = t;
     evt.button = button;
     evt.x = pt.x;
     evt.y = pt.y;
-    [d handleUIEvent:(union pce_event *) &evt];
+    [d handleUIEvent:(union sg_event *) &evt];
 }
 
 @implementation GDisplay (Private)
@@ -217,7 +217,7 @@ static void handleMouse(GDisplay *d, NSEvent *e, pce_event_type_t t, int button)
 
 - (void)stateChanged {
     unsigned status;
-    struct pce_event_status evt;
+    struct sg_event_status evt;
     if ([NSApp isHidden]) {
         status = 0;
     } else if (mode_ == GDisplayWindow) {
@@ -230,7 +230,7 @@ static void handleMouse(GDisplay *d, NSEvent *e, pce_event_type_t t, int button)
     }
     evt.type = SG_EVENT_STATUS;
     evt.status = status;
-    sg_sys_event((union pce_event *) &evt);
+    sg_sys_event((union sg_event *) &evt);
 }
 
 @end
@@ -390,7 +390,7 @@ error:
         [self showWindow:sender];
 }
 
-- (void)handleUIEvent:(union pce_event *)event {
+- (void)handleUIEvent:(union sg_event *)event {
     [self lock];
     sg_sys_event(event);
     [self unlock];
@@ -407,11 +407,11 @@ error:
     if (frameChanged_) {
         [context_ update];
         glViewport(0, 0, width_, height_);
-        struct pce_event_resize evt;
+        struct sg_event_resize evt;
         evt.type = SG_EVENT_RESIZE;
         evt.width = width_;
         evt.height = height_;
-        sg_sys_event((union pce_event *) &evt);
+        sg_sys_event((union sg_event *) &evt);
     }
     sg_sys_draw();
     [context_ flushBuffer];

sg/osx/GView.m
@@ -2,15 +2,15 @@
 #import "GView.h"
 #import "GDisplay.h"
 
-static void handleMouse(GView *v, NSEvent *e, pce_event_type_t t, int button)
+static void handleMouse(GView *v, NSEvent *e, sg_event_type_t t, int button)
 {
     NSPoint pt = [v convertPoint:[e locationInWindow] fromView:nil];
-    struct pce_event_mouse evt;
+    struct sg_event_mouse evt;
     evt.type = t;
     evt.button = button;
     evt.x = pt.x;
     evt.y = pt.y;
-    [v->display_ handleUIEvent:(union pce_event *) &evt];
+    [v->display_ handleUIEvent:(union sg_event *) &evt];
 }
 
 @implementation GView

sg/sdl/sdl.c
@@ -81,7 +81,7 @@ static SDL_Surface *sdl_surface;
 static void
 init(int argc, char *argv[])
 {
-    struct pce_event_resize rsz;
+    struct sg_event_resize rsz;
     struct sg_game_info gameinfo;
     int opt;
 
@@ -112,24 +112,24 @@ init(int argc, char *argv[])
     rsz.type = SG_EVENT_RESIZE;
     rsz.width = sdl_surface->w;
     rsz.height = sdl_surface->h;
-    sg_sys_event((union pce_event *) &rsz);
+    sg_sys_event((union sg_event *) &rsz);
 }
 
 static void
 sdl_event_mousemove(SDL_MouseMotionEvent *e)
 {
-    struct pce_event_mouse ee;
+    struct sg_event_mouse ee;
     ee.type = SG_EVENT_MMOVE;
     ee.button = -1;
     ee.x = e->x;
     ee.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union pce_event *) &ee);
+    sg_sys_event((union sg_event *) &ee);
 }
 
 static void
 sdl_event_mousebutton(SDL_MouseButtonEvent *e)
 {
-    struct pce_event_mouse ee;
+    struct sg_event_mouse ee;
     ee.type = e->type == SDL_MOUSEBUTTONDOWN ?
         SG_EVENT_MDOWN : SG_EVENT_MUP;
     switch (e->button) {
@@ -142,29 +142,29 @@ sdl_event_mousebutton(SDL_MouseButtonEvent *e)
     }
     ee.x = e->x;
     ee.y = sg_window_height - 1 - e->y;
-    sg_sys_event((union pce_event *) &ee);
+    sg_sys_event((union sg_event *) &ee);
 }
 
 static void
 sdl_event_key(SDL_KeyboardEvent *e)
 {
-    struct pce_event_key ee;
+    struct sg_event_key ee;
     ee.type = e->type == SDL_KEYDOWN ?
         SG_EVENT_KDOWN : SG_EVENT_KUP;
     ee.key = -1;
-    sg_sys_event((union pce_event *) &ee);
+    sg_sys_event((union sg_event *) &ee);
 }
 
 static void
 sdl_event_resize(SDL_ResizeEvent *e)
 {
-    struct pce_event_resize ee;
+    struct sg_event_resize ee;
     sg_window_width = e->w;
     sg_window_height = e->h;
     ee.type = SG_EVENT_RESIZE;
     ee.width = e->w;
     ee.height = e->h;
-    sg_sys_event((union pce_event *) &ee);
+    sg_sys_event((union sg_event *) &ee);
 }
 
 static void

sg/windows/windows.c
@@ -75,7 +75,7 @@ static int sg_height;
 
 static void handleResize(int width, int height)
 {
-    struct pce_event_resize e;
+    struct sg_event_resize e;
     if (!height)
         height = 1;
     if (!width)
@@ -84,7 +84,7 @@ static void handleResize(int width, int height)
     e.width = width;
     e.height = height;
     sg_height = height;
-    sg_sys_event((union pce_event *) &e);
+    sg_sys_event((union sg_event *) &e);
 }
 
 static void killGLWindow()
@@ -217,9 +217,9 @@ BOOL createWindow(int nCmdShow)
     return TRUE;
 }
 
-static void handleKey(int code, pce_event_type_t t)
+static void handleKey(int code, sg_event_type_t t)
 {
-    struct pce_event_key e;
+    struct sg_event_key e;
     int hcode;
     if (code < 0 || code > 255)
         return;
@@ -228,17 +228,17 @@ static void handleKey(int code, pce_event_type_t t)
         return;
     e.type = t;
     e.key = hcode;
-    sg_sys_event((union pce_event *) &e);
+    sg_sys_event((union sg_event *) &e);
 }
 
-static void handleMouse(int param, pce_event_type_t t, int button)
+static void handleMouse(int param, sg_event_type_t t, int button)
 {
-    struct pce_event_mouse e;
+    struct sg_event_mouse e;
     e.type = t;
     e.x = LOWORD(param);
     e.y = sg_height - 1 - HIWORD(param);
     e.button = button;
-    sg_sys_event((union pce_event *) &e);
+    sg_sys_event((union sg_event *) &e);
 }
 
 static LRESULT CALLBACK wndProc(

sgpp/ui/screen.cpp
@@ -59,21 +59,21 @@ void sg_game_getinfo(struct sg_game_info *info)
     (void) info;
 }
 
-static void mouse_event(union pce_event *evt, EventType t)
+static void mouse_event(union sg_event *evt, EventType t)
 {
-    struct pce_event_mouse &me = evt->mouse;
+    struct sg_event_mouse &me = evt->mouse;
     MouseEvent ue(t, me.button, me.x, me.y);
     current_screen->handleEvent(ue);
 }
 
-static void key_event(union pce_event *evt, EventType t)
+static void key_event(union sg_event *evt, EventType t)
 {
-    struct pce_event_key &ke = evt->key;
+    struct sg_event_key &ke = evt->key;
     KeyEvent ue(t, ke.key);
     current_screen->handleEvent(ue);
 }
 
-void sg_game_event(union pce_event *evt)
+void sg_game_event(union sg_event *evt)
 {
     if (!current_screen) {
         handle_nullscreen();