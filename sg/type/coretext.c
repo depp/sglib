7b432d62ee9c8e14767818bd1641829c365a6cdc
Dietrich Epp <depp@zdome.net>
2013-12-08 12:36:41 -0800

    Update Core Text layout to use new API

--- sg/type/coretext.c
+++ sg/type/coretext.c
@@ -1,13 +1,16 @@
-/* Copyright 2012 Dietrich Epp.
+/* Copyright 2012-2013 Dietrich Epp.
    This file is part of SGLib.  SGLib is licensed under the terms of the
    2-clause BSD license.  For more information, see LICENSE.txt. */
-#include "impl.h"
+#include "sg/error.h"
 #include "sg/pixbuf.h"
+#include "sg/type.h"
 #include <ApplicationServices/ApplicationServices.h>
 
 static const float FRAME_HEIGHT = 2048.0f;
 
-struct sg_layout_impl {
+struct sg_textbitmap {
+    sg_textalign_t alignment;
+
     /* Simple (no wrap) version */
     CTLineRef line;
 
@@ -16,102 +19,206 @@ struct sg_layout_impl {
     CTFrameRef frame;
 };
 
-struct sg_layout_impl *
-sg_layout_impl_new(struct sg_layout *lp)
+/* Create a string attribute dictionary from the given parameters.  */
+static CFDictionaryRef
+sg_textbitmap_makeattr(const char *fontname, double fontsize,
+                       sg_textalign_t alignment)
 {
-    struct sg_layout_impl *li;
-    CFStringRef string = NULL;
     CGColorRef white = NULL;
+    CFStringRef fname = NULL;
     CTFontRef font = NULL;
-    CFStringRef keys[2], fname;
-    CFTypeRef vals[2];
+    CTTextAlignment ctAlignment;
+    CTParagraphStyleSetting setting;
+    CTParagraphStyleRef paragraphStyle = NULL;
+    CFStringRef keys[3];
+    CFTypeRef vals[3];
     CFDictionaryRef attr = NULL;
-    CFAttributedStringRef attrstring = NULL;
-    CTLineRef line = NULL;
-    CTFramesetterRef framesetter = NULL;
-    CTFrameRef frame = NULL;
-    CGRect bounds;
-    CGMutablePathRef path;
 
-    string = CFStringCreateWithBytes(
-        kCFAllocatorDefault, (const UInt8 *) lp->text, lp->textlen,
-        kCFStringEncodingUTF8, false);
+    fontname = "Helvetica";
+
     white = CGColorCreateGenericGray(1.0f, 1.0f);
-    if (lp->family) {
-        fname = CFStringCreateWithBytes(
-            NULL, (UInt8 *) lp->family, strlen(lp->family),
-            kCFStringEncodingUTF8, false);
-        font = CTFontCreateWithName(fname, lp->size, NULL);
-        CFRelease(fname);
+    if (!white) goto error;
+
+    fname = CFStringCreateWithBytes(
+        NULL, (UInt8 *) fontname, strlen(fontname),
+        kCFStringEncodingUTF8, false);
+    if (!fname) goto error;
+    font = CTFontCreateWithName(fname, fontsize, NULL);
+    if (!font) goto error;
+
+    switch (alignment) {
+    default:
+    case SG_TEXTALIGN_LEFT: ctAlignment = kCTLeftTextAlignment; break;
+    case SG_TEXTALIGN_RIGHT: ctAlignment = kCTRightTextAlignment; break;
+    case SG_TEXTALIGN_CENTER: ctAlignment = kCTCenterTextAlignment; break;
     }
-    font = CTFontCreateWithName(CFSTR("Helvetica"), lp->size, NULL);
+    setting.spec = kCTParagraphStyleSpecifierAlignment;
+    setting.valueSize = sizeof(ctAlignment);
+    setting.value = &ctAlignment;
+    paragraphStyle = CTParagraphStyleCreate(&setting, 1);
+    if (!paragraphStyle) goto error;
 
     keys[0] = kCTFontAttributeName;
     vals[0] = font;
     keys[1] = kCTForegroundColorAttributeName;
     vals[1] = white;
+    keys[2] = kCTParagraphStyleAttributeName;
+    vals[2] = paragraphStyle;
 
     attr = CFDictionaryCreate(
         kCFAllocatorDefault, (const void **) keys, (const void **) vals, 2,
         &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
-    attrstring = CFAttributedStringCreate(kCFAllocatorDefault, string, attr);
+    if (!attr) goto error;
 
-    if (lp->width >= 0) {
-        bounds = CGRectMake(0.0, 0.0, lp->width, FRAME_HEIGHT);
-        path = CGPathCreateMutable();
-        CGPathAddRect(path, NULL, bounds);
-        framesetter = CTFramesetterCreateWithAttributedString(attrstring);
-        frame = CTFramesetterCreateFrame(
-            framesetter, CFRangeMake(0, CFStringGetLength(string)),
-            path, NULL);
-        CFRelease(path);
+    CFRelease(paragraphStyle);
+    CFRelease(font);
+    CFRelease(fname);
+    CFRelease(white);
+    return attr;
+
+error:
+    if (attr) CFRelease(attr);
+    if (paragraphStyle) CFRelease(paragraphStyle);
+    if (font) CFRelease(font);
+    if (fname) CFRelease(fname);
+    if (white) CFRelease(white);
+    return NULL;
+}
+
+/* Create an attributed string from the given attributes and UTF-8 text.  */
+static CFAttributedStringRef
+sg_textbitmap_makestring(const char *text, size_t textlen,
+                         CFDictionaryRef attributes)
+{
+    CFStringRef string = NULL;
+    CFAttributedStringRef attrstring = NULL;
+
+    string = CFStringCreateWithBytes(
+        kCFAllocatorDefault, (const UInt8 *) text, textlen,
+        kCFStringEncodingUTF8, false);
+    if (!string) goto error;
+
+    attrstring = CFAttributedStringCreate(
+        kCFAllocatorDefault, string, attributes);
+    if (!attrstring) goto error;
+
+    CFRelease(string);
+    return attrstring;
+
+error:
+    if (string) CFRelease(string);
+    if (attrstring) CFRelease(attrstring);
+    return NULL;
+}
+
+/* Create a frame based text bitmap.  */
+static int
+sg_textbitmap_makeframe(struct sg_textbitmap *bitmap,
+                        CFAttributedStringRef string, double width)
+{
+    CGRect bounds;
+    CTFramesetterRef framesetter = NULL;
+    CTFrameRef frame = NULL;
+    CGMutablePathRef path = NULL;
+
+    bounds = CGRectMake(0.0, 0.0, width, FRAME_HEIGHT);
+    path = CGPathCreateMutable();
+    if (!path) goto error;
+    CGPathAddRect(path, NULL, bounds);
+    framesetter = CTFramesetterCreateWithAttributedString(string);
+    if (!framesetter) goto error;
+    frame = CTFramesetterCreateFrame(
+        framesetter, CFRangeMake(0, CFAttributedStringGetLength(string)),
+        path, NULL);
+    if (!frame) goto error;
+
+    CFRelease(path);
+    bitmap->framesetter = framesetter;
+    bitmap->frame = frame;
+    return 0;
+
+error:
+    if (frame) CFRelease(frame);
+    if (framesetter) CFRelease(framesetter);
+    if (path) CFRelease(path);
+    return -1;
+}
+
+/* Create a line based text bitmap.  */
+static int
+sg_textbitmap_makeline(struct sg_textbitmap *bitmap,
+                       CFAttributedStringRef string)
+{
+    CTLineRef line;
+    line = CTLineCreateWithAttributedString(string);
+    if (!line)
+        return -1;
+    bitmap->line = line;
+    return 0;
+}
+
+struct sg_textbitmap *
+sg_textbitmap_new_simple(const char *text, size_t textlen,
+                         const char *fontname, double fontsize,
+                         sg_textalign_t alignment, double width,
+                         struct sg_error **err)
+{
+    struct sg_textbitmap *bitmap = NULL;
+    CFDictionaryRef attr = NULL;
+    CFAttributedStringRef str = NULL;
+
+    attr = sg_textbitmap_makeattr(fontname, fontsize, alignment);
+    if (!attr) goto error;
+    str = sg_textbitmap_makestring(text, textlen, attr);
+    if (!str) goto error;
+
+    bitmap = calloc(1, sizeof(*bitmap));
+    if (!bitmap) goto error;
+
+    bitmap->alignment = alignment;
+    if (width > 0.0) {
+        if (sg_textbitmap_makeframe(bitmap, str, width))
+            goto error;
     } else {
-        line = CTLineCreateWithAttributedString(attrstring);
+        if (sg_textbitmap_makeline(bitmap, str))
+            goto error;
     }
 
-    CFRelease(attrstring);
+    CFRelease(str);
     CFRelease(attr);
-    CFRelease(font);
-    CFRelease(white);
-    CFRelease(string);
+    return bitmap;
 
-    li = malloc(sizeof(*li));
-    if (!li) abort();
-    li->line = line;
-    li->framesetter = framesetter;
-    li->frame = frame;
-
-    return li;
+error:
+    if (str) CFRelease(str);
+    if (attr) CFRelease(attr);
+    free(bitmap);
+    sg_error_nomem(err);
+    return NULL;
 }
 
 void
-sg_layout_impl_free(struct sg_layout_impl *li)
+sg_textbitmap_free(struct sg_textbitmap *bitmap)
 {
-    if (li->line)
-        CFRelease(li->line);
-    if (li->framesetter)
-        CFRelease(li->framesetter);
-    if (li->frame)
-        CFRelease(li->frame);
-    free(li);
+    if (bitmap->line)
+        CFRelease(bitmap->line);
+    if (bitmap->framesetter)
+        CFRelease(bitmap->framesetter);
+    if (bitmap->frame)
+        CFRelease(bitmap->frame);
+    free(bitmap);
 }
 
 static void
-sg_layout_copyrect(struct sg_layout_rect *dest, CGRect *src)
+sg_textbitmap_copyrect(struct sg_textrect *dest, CGRect *src)
 {
-    int x1, x2, y1, y2;
-    x1 = floorf(CGRectGetMinX(*src));
-    y1 = floorf(CGRectGetMinY(*src));
-    x2 = ceilf(CGRectGetMaxX(*src));
-    y2 = ceilf(CGRectGetMaxY(*src));
-    dest->x = x1;
-    dest->y = y1;
-    dest->width = x2 - x1;
-    dest->height = y2 - y1;
+    dest->x0 = floorf(CGRectGetMinX(*src));
+    dest->y0 = floorf(CGRectGetMinY(*src));
+    dest->x1 = ceilf(CGRectGetMaxX(*src));
+    dest->y1 = ceilf(CGRectGetMaxY(*src));
 }
 
 static CGContextRef
-sg_layout_dummycontext(void)
+sg_textbitmap_dummycontext(void)
 {
     static CGContextRef dummy_context;
     static unsigned char dummy_context_data;
@@ -128,7 +235,7 @@ sg_layout_dummycontext(void)
 }
 
 static CGRect
-sg_layout_ctline_pixbounds(CTLineRef line, CGPoint off, CGContextRef cxt)
+sg_textbitmap_ctline_pixbounds(CTLineRef line, CGPoint off, CGContextRef cxt)
 {
     CGRect r = CTLineGetImageBounds(line, cxt);
     r.origin.x += off.x;
@@ -137,7 +244,7 @@ sg_layout_ctline_pixbounds(CTLineRef line, CGPoint off, CGContextRef cxt)
 }
 
 static CGRect
-sg_layout_ctline_logbounds(CTLineRef line, CGPoint off)
+sg_textbitmap_ctline_logbounds(CTLineRef line, CGPoint off)
 {
     double lwidth;
     CGFloat ascent, descent;
@@ -150,75 +257,94 @@ sg_layout_ctline_logbounds(CTLineRef line, CGPoint off)
     return r;
 }
 
-void
-sg_layout_impl_calcbounds(struct sg_layout_impl *li,
-                          struct sg_layout_bounds *b)
+int
+sg_textbitmap_getmetrics(struct sg_textbitmap *bitmap,
+                         struct sg_textlayout_metrics *metrics,
+                         struct sg_error **err)
 {
-    CGContextRef cxt = sg_layout_dummycontext();
+    CGContextRef cxt = sg_textbitmap_dummycontext();
     CGRect pixbounds, logbounds, r;
     CFArrayRef array;
     CGPoint *origins, origin;
     CFIndex n, i;
     CTLineRef line;
-    if (li->frame) {
-        array = CTFrameGetLines(li->frame);
+    if (bitmap->frame) {
+        array = CTFrameGetLines(bitmap->frame);
+        if (!array) goto nomem;
         n = CFArrayGetCount(array);
         if (n > 0) {
             origins = malloc(sizeof(*origins) * n);
-            if (!origins) abort();
-            CTFrameGetLineOrigins(li->frame, CFRangeMake(0, n), origins);
+            if (!origins) goto nomem;
+            CTFrameGetLineOrigins(bitmap->frame, CFRangeMake(0, n), origins);
             line = CFArrayGetValueAtIndex(array, 0);
             origin = origins[0];
-            pixbounds = sg_layout_ctline_pixbounds(line, origin, cxt);
-            logbounds = sg_layout_ctline_logbounds(line, origin);
+            pixbounds = sg_textbitmap_ctline_pixbounds(line, origin, cxt);
+            logbounds = sg_textbitmap_ctline_logbounds(line, origin);
             for (i = 1; i < n; ++i) {
                 line = CFArrayGetValueAtIndex(array, i);
                 origin = origins[i];
-                r = sg_layout_ctline_pixbounds(line, origin, cxt);
+                r = sg_textbitmap_ctline_pixbounds(line, origin, cxt);
                 pixbounds = CGRectUnion(pixbounds, r);
-                r = sg_layout_ctline_logbounds(line, origin);
+                r = sg_textbitmap_ctline_logbounds(line, origin);
                 logbounds = CGRectUnion(logbounds, r);
             }
-            b->x = (int) floorf(origins[0].x + 0.5f);
-            b->y = (int) floorf(origins[0].y + 0.5f);
+            metrics->origin.x = (int) floorf(origins[0].x + 0.5f);
+            metrics->origin.y = (int) floorf(origins[0].y + 0.5f);
             free(origins);
         } else {
             pixbounds = CGRectZero;
             logbounds = CGRectZero;
-            b->x = 0;
-            b->y = 0;
+            metrics->origin.x = 0;
+            metrics->origin.y = 0;
         }
     } else {
-        line = li->line;
+        line = bitmap->line;
         pixbounds = CTLineGetImageBounds(line, cxt);
-        logbounds = sg_layout_ctline_logbounds(line, CGPointZero);
-        b->x = 0;
-        b->y = 0;
+        logbounds = sg_textbitmap_ctline_logbounds(line, CGPointZero);
+        metrics->origin.x = 0;
+        metrics->origin.y = 0;
     }
-    sg_layout_copyrect(&b->pixel, &pixbounds);
-    sg_layout_copyrect(&b->logical, &logbounds);
+    sg_textbitmap_copyrect(&metrics->pixel, &pixbounds);
+    sg_textbitmap_copyrect(&metrics->logical, &logbounds);
+    return 0;
+
+nomem:
+    sg_error_nomem(err);
+    return -1;
 }
 
-void
-sg_layout_impl_render(struct sg_layout_impl *li, struct sg_pixbuf *pbuf,
-                      int xoff, int yoff)
+int
+sg_textbitmap_render(struct sg_textbitmap *bitmap,
+                     struct sg_pixbuf *pixbuf,
+                     struct sg_textpoint offset,
+                     struct sg_error **err)
 {
     CGColorSpaceRef color_space = NULL;
     CGContextRef context = NULL;
 
     color_space = CGColorSpaceCreateWithName(kCGColorSpaceGenericGray);
+    if (!color_space) goto nomem;
     context = CGBitmapContextCreate(
-        pbuf->data, pbuf->pwidth, pbuf->pheight, 8, pbuf->rowbytes,
+        pixbuf->data, pixbuf->pwidth, pixbuf->pheight, 8, pixbuf->rowbytes,
         color_space, 0);
+    if (!context) goto nomem;
 
-    if (li->frame) {
-        CGContextTranslateCTM(context, xoff, yoff);
-        CTFrameDraw(li->frame, context);
+    if (bitmap->frame) {
+        CGContextTranslateCTM(context, offset.x, offset.y);
+        CTFrameDraw(bitmap->frame, context);
     } else {
-        CGContextSetTextPosition(context, xoff, yoff);
-        CTLineDraw(li->line, context);
+        CGContextSetTextPosition(context, offset.x, offset.y);
+        CTLineDraw(bitmap->line, context);
     }
 
     CFRelease(color_space);
     CFRelease(context);
+
+    return 0;
+
+nomem:
+    sg_error_nomem(err);
+    if (color_space) CFRelease(color_space);
+    if (context) CFRelease(context);
+    return -1;
 }
