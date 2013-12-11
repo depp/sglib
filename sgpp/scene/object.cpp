897267f98a0374c0e0414a1b0e3a782eb0fe171e
Dietrich Epp <depp@zdome.net>
2013-12-10 22:41:34 -0800

    Remove C++ wrapper

--- sgpp/scene/object.cpp
+++
@@ -1,7 +0,0 @@
-/* Copyright 2012 Dietrich Epp.
-   This file is part of SGLib.  SGLib is licensed under the terms of the
-   2-clause BSD license.  For more information, see LICENSE.txt. */
-#include "sgpp/scene/object.hpp"
-
-Scene::Object::~Object()
-{ }
