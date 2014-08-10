e800ec70208dbdd3fc5915e653aa037fa52f854a
Dietrich Epp <depp@zdome.net>
2014-08-10 15:28:37 -0700

    clean up game entry points
    * Use double instead of fixed for window size
    * Pass only width and height to draw()
    * Expose audio system (callback, audio_start())
    * Add Doxygen documentation to header

--- include/sg/entry.h
+++ include/sg/entry.h
@@ -6,7 +6,8 @@
 /* Entry points: the layer that connects library code, game code, and
    platform code.  */
 #include <stdarg.h>
-#include "sg/defs.h"
+#include "libpce/attribute.h"
+#include "sg/audio.h"
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -14,81 +15,171 @@ union sg_event;
 struct sg_error;
 struct sg_logger;
 
-#define SG_GAME_ASPECT_SCALE 0x10000
+/**
+ * @file sg/entry.h
+ *
+ * @brief Game entry points.
+ *
+ * This layer connects the library code with the game code.  This file
+ * defines functions both in the game code (which must be implemented
+ * by the game) and functions in the library code.
+ */
 
+/**
+ * @brief Game startup information.
+ */
 struct sg_game_info {
-    /* Minimum window size */
-    int min_width, min_height;
+    /** @brief The minimum window width.  */
+    int min_width;
+    /** @brief The minimum window height.  */
+    int min_height;
 
-    /* Default window size */
-    int default_width, default_height;
+    /** @brief The suggested default window width.  */
+    int default_width;
+    /** @brief The suggested default window height.  */
+    int default_height;
 
-    /* Minimum and maximum aspect ratio, multiplied by
-       SG_GAME_ASPECT_SCALE (i.e., 16.16 fixed point) */
-    int min_aspect, max_aspect;
+    /** @brief The minimum window aspect ratio, width divided by
+        height.  */
+    double min_aspect;
+    /** @brief The maximum window aspect ratio, width divided by
+        height.  */
+    double max_aspect;
 };
 
-/* ===== Game exports =====
-
-   These are provided by the game code.  These should NOT be called
-   directly by platform code, platform code should use the wrappers
-   below.  */
-
-/* Called once after the game launches.  No game entry points will be
-   used before this is called.  */
+/**
+ * @brief Handle game startup.
+ *
+ * This is a game entry point, and must be implemented.  This function
+ * will be called exactly once, before any other entry points.  The
+ * OpenGL context will not be valid.
+ */
 void
 sg_game_init(void);
 
-/* Get information about the game.  */
+/**
+ * @brief Handle request for game startup information.
+ *
+ * This is a game entry point, and must be implemented.  The OpenGL
+ * context will not be valid.
+ *
+ * @param info When called, this contains the default startup
+ * information.  This may be modified or left unmodified.
+ */
 void
 sg_game_getinfo(struct sg_game_info *info);
 
-/* Called for every event.  The OpenGL context may not be valid, you
-   should not attempt to draw to the screen, load textures, etc.  */
+/**
+ * @brief Handle an event.
+ *
+ * This is a game entry point, and must be implemented.  The OpenGL
+ * context will not be valid except for certain event types.
+ *
+ * @brief evt The event to handle.
+ */
 void
 sg_game_event(union sg_event *evt);
 
-/* Draw the scene.  The OpenGL context will be valid.  The x, y,
-   width, and height parameters specify the current viewport, the game
-   should not draw outside this area.  The msecs parameter is the
-   number of milliseconds since an arbitrary time.  Note that msecs
-   overflows every 47 days.  */
+/**
+ * @brief Handle screen redraw.
+ *
+ * This is a game entry point, and must be implemented.  OpenGL
+ * functions may be used.
+ *
+ * @param width The viewport width.
+ * @param height The viewport height.
+ * @param msec The current time, in milliseconds since startup.
+ */
 void
-sg_game_draw(int x, int y, int width, int height, unsigned msec);
+sg_game_draw(int width, int height, unsigned msec);
 
-/* Called at most once before the process exits.  */
+/**
+ * @brief Get audio data.
+ *
+ * This is a game entry point, and must be implemented.  The OpenGL
+ * context will not be valid.
+ *
+ * @param buffer The audio buffer to fill.  The buffer size is taken
+ * from the audio initialization event.  The buffer format is always
+ * interleaved stereo with 32-bit floating pont values.
+ *
+ * @param The current time, in milliseconds since startup.
+ *
+ * @return A result code for the audio stream.
+ */
+sg_audio_result_t
+sg_game_audio(float *buffer, int msec);
+
+/**
+ * @brief Handle game shutdown.
+ *
+ * This is a game entry point, and must be implemented.  The OpenGL
+ * context will not be valid.  This function may be called at most
+ * once, before the program shuts down.  No other entry points will be
+ * called after this one.
+ */
 void
 sg_game_destroy(void);
 
-/* ===== Platform exports =====
+/**
+ * @brief Start the audio system.
+ *
+ * If successful, this will send an audio initialization event, then
+ * start calling the ::sg_game_audio callback.  The audio system may
+ * be started on another thread and this function may return before
+ * the audio system is fully initialized.
+ *
+ * @return 0 if the audio system was started, nonzero if the audio
+ * system could not be started.
+ */
+int
+sg_sys_audio_start(void);
 
-   These are called by game and library code.  Each platform must
-   implement these.  */
-
-/* Causes the game to exit.  Additional events or drawing commands may
-   be issued before the game exits.  */
+/**
+ * @brief Stop the audio system.
+ *
+ * This will wait for the audio system to shut down, which requires
+ * handling the audio stop event.
+ */
 void
-sg_platform_quit(void);
+sg_sys_audio_stop(void);
 
-/* Signal a serious error.  */
+/**
+ * @brief Quit the program.
+ */
 void
-sg_platform_failf(const char *fmt, ...);
+sg_sys_quit(void);
 
-/* Signal a serious error.  */
+/**
+ * @brief Display the given error message and exit the program
+ * immediately.
+ *
+ * @param msg The message to display, a NUL-terminated UTF-8 string.
+ */
+PCE_ATTR_NORETURN
 void
-sg_platform_failv(const char *fmt, va_list ap);
+sg_sys_abort(const char *msg);
 
-/* Signal a serious error.  */
+/**
+ * @brief Display the given error message and exit the program
+ * immediately.
+ *
+ * @param msg The message to display, a NUL-terminated UTF-8 string.
+ */
+PCE_ATTR_NORETURN
+PCE_ATTR_FORMAT(printf, 1, 2)
 void
-sg_platform_faile(struct sg_error *err);
+sg_sys_abortf(const char *msg, ...);
 
-/* Return immediately from the game code to the platform code and
-   display the queued error message to the user.  This should only be
-   used after a serious error has already been signaled using
-   sg_platform_failf or sg_platform_faile.  */
-__attribute__((noreturn))
+/**
+ * @brief Display the given error message and exit the program
+ * immediately.
+ *
+ * @param msg The message to display, a NUL-terminated UTF-8 string.
+ */
+PCE_ATTR_NORETURN
 void
-sg_platform_return(void);
+sg_sys_abortv(const char *msg, va_list ap);
 
 #ifdef __cplusplus
 }
