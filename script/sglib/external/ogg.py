5f13c1bb523f527503c0e7c2e660719dafb4ac5b
Dietrich Epp <depp@zdome.net>
2014-10-22 20:48:48 -0700

    build: build bundled Ogg and Vorbis libraries

--- script/sglib/external/ogg.py
+++ script/sglib/external/ogg.py
@@ -2,9 +2,33 @@
 # This file is part of SGLib.  SGLib is licensed under the terms of the
 # 2-clause BSD license.  For more information, see LICENSE.txt.
 from d3build.module import ExternalModule
+from d3build.error import try_config
+from d3build.source import SourceList
+import os
+
+def pkg_config(build):
+    return None, [], {'public': [build.env.pkg_config('ogg')]}
+
+# Note: should set:
+# -ffast-math
+# -falign-loops=16
+
+def bundled(build):
+    env = build.env
+    path = env.find_library('^libogg-[0-9.]+$')
+    src = SourceList(path=path, sources='''
+    src/bitwise.c
+    src/framing.c
+    ogg/ogg.h
+    ogg/os_types.h
+    ''')
+    varsets = [
+        env.header_path(os.path.join(path, 'include'), system=True),
+    ]
+    return path, src.sources, {'public': varsets}
 
 def _configure(build):
-    return None, [], {'public': [build.env.pkg_config('ogg')]}
+    return try_config([build], pkg_config, bundled)
 
 module = ExternalModule(
     name='Ogg bitstream library',
--- script/sglib/external/vorbis.py
+++ script/sglib/external/vorbis.py
@@ -2,9 +2,57 @@
 # This file is part of SGLib.  SGLib is licensed under the terms of the
 # 2-clause BSD license.  For more information, see LICENSE.txt.
 from d3build.module import ExternalModule
+from d3build.error import try_config
+from d3build.source import SourceList
+import os
+from . import ogg
+
+def pkg_config(build):
+    return None, [], {'public': [build.env.pkg_config('vorbis')]}
+
+# Note: should set:
+# -ffast-math
+# -falign-loops=16
+# -D__MACOSX__
+
+def bundled(build):
+    env = build.env
+    path = env.find_library('^libvorbis-[0-9.]+$')
+    src = SourceList(path=path)
+    src.add(path='lib', sources='''
+    analysis.c
+    bitrate.c
+    block.c
+    codebook.c
+    envelope.c
+    floor0.c
+    floor1.c
+    info.c
+    lookup.c
+    lpc.c
+    lsp.c
+    mapping0.c
+    mdct.c
+    psy.c
+    registry.c
+    res0.c
+    sharedbook.c
+    smallft.c
+    synthesis.c
+    window.c
+    ''')
+    return path, src.sources, {
+        'public': [
+            env.header_path(os.path.join(path, 'include'), system=True),
+        ],
+        'private': [
+            env.header_path(os.path.join(path, 'lib')),
+            ogg.module
+        ],
+    }
 
 def _configure(build):
-    return None, [], {'public': [build.env.pkg_config('vorbis')]}
+    return try_config([build], pkg_config, bundled)
 
 module = ExternalModule(
     name='Vorbis codec library',
