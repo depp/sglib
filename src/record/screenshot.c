eeb4ff756570db90e5e33e3d0a875c87a4272dc6
Dietrich Epp <depp@zdome.net>
2014-12-05 03:57:26 -0800

    change screenshot path

--- src/record/screenshot.c
+++ src/record/screenshot.c
@@ -12,10 +12,10 @@
 void
 sg_screenshot_write(const void *ptr, int width, int height)
 {
-    char name[SG_DATE_LEN + 4];
+    char name[SG_DATE_LEN + 16]; /* screenshot/.png\0 */
     struct sg_error *err = NULL;
     struct sg_pixbuf pbuf;
-    int r;
+    int len1, len2, r;
 
     pbuf.data = (void *) ptr;
     pbuf.format = SG_RGBX;
@@ -23,12 +23,16 @@ sg_screenshot_write(const void *ptr, int width, int height)
     pbuf.height = height;
     pbuf.rowbytes = width * 4;
 
-    r = sg_clock_getdate(name, 1);
-    assert(r >= 0 && r < SG_DATE_LEN);
-    memcpy(name + r, ".png", 5);
-    r = sg_pixbuf_writepng(&pbuf, name, r + 4, &err);
+    len1 = strlen("screenshot/");
+    memcpy(name, "screenshot/", len1);
+    len2 = sg_clock_getdate(name + len1, 1);
+    assert(len2 >= 0 && len2 < SG_DATE_LEN);
+    memcpy(name + len1 + len2, ".png", 5);
+    r = sg_pixbuf_writepng(&pbuf, name, len1 + len2 + 4, &err);
     if (r) {
-        sg_logerrs(SG_LOG_ERROR, err, "could not save screenshot");
+        sg_logerrs(SG_LOG_ERROR, err, "Could not save screenshot.");
         sg_error_clear(&err);
+    } else {
+        sg_logf(SG_LOG_INFO, "Saved screenshot: %s", name);
     }
 }
